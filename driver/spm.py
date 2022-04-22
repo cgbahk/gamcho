aa085e7e3d69e896e5d4c821cb11629ef39289d8
Cheongyo Bahk <cg.bahk@gmail.com>
2022-04-22 14:29:25 +0900

    [spm] Add conjecture check

--- driver/spm.py
+++ driver/spm.py
@@ -4,6 +4,17 @@ import click
 import sentencepiece as spm
 
 
+def check_conjecture(processor: spm.SentencePieceProcessor):
+    # Version(?) check
+    assert processor.get_encoder_version() == 0
+
+    # Size check
+    size = len(processor)
+    assert size == processor.get_piece_size()
+    assert size == processor.vocab_size()
+    assert size == processor.piece_size()
+
+
 @click.group()
 def cli():
     pass
@@ -16,7 +27,13 @@ def inspect(spm_model_path):
     assert spm_model_path.is_file()
 
     processor = spm.SentencePieceProcessor(str(spm_model_path))
+    check_conjecture(processor)
     print(f"length of processor: {len(processor)}")
+
+    print(f"unk id: {processor.unk_id()}")
+    print(f"bos id: {processor.bos_id()}")
+    print(f"eos id: {processor.eos_id()}")
+    print(f"pad id: {processor.pad_id()}")
 
     # TODO This makes too long output. Let's make tabular.
     def print_tokens_between(beg, end):
