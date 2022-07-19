09233479b0e1cf6046e956cc94469e35eacd16ce
Cheongyo Bahk <cg.bahk@gmail.com>
2022-07-19 11:31:24 +0900

    [altair] Add automatic chart concat

--- gamcho/altair_.py
+++ gamcho/altair_.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 import altair as alt
+from math import sqrt
 
 
 def get_heatmap_for_2D(tensor):
@@ -20,3 +21,38 @@ def get_heatmap_for_2D(tensor):
     frame = pd.DataFrame({'column': col.ravel(), 'row': row.ravel(), 'value': tensor.ravel()})
 
     return alt.Chart(frame).mark_rect().encode(x="column:O", y="row:O", color="value:Q")
+
+
+def auto_concat(charts, *, height=None, width=None):
+    """
+    charts: List of alt.Chart
+    height/width: Desired count of chart for each
+                  By default, infer automatic value with golden ratio
+    """
+    num_chart = len(charts)
+    assert num_chart > 0
+
+    if (height, width) == (None, None):
+        height, width = 0, 0
+
+        golden_ratio = (1 + sqrt(5)) / 2
+
+        while height * width < num_chart:
+            height += 1
+            width = round(golden_ratio * height)
+
+    # TODO Support other case of (height, width): (None, Valid), (Valid, None)
+
+    assert isinstance(height, int)
+    assert height > 0
+
+    assert isinstance(width, int)
+    assert width > 0
+
+    assert height * width >= num_chart
+
+    ret = alt.vconcat()
+    for vertical_idx in range(height):
+        ret &= alt.hconcat(*charts[vertical_idx * width:(vertical_idx + 1) * width])
+
+    return ret
